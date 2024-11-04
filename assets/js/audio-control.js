8687569ec0d007171805af32c67ed004da5929ec
CoryWBoris <CoryWBoris@gmail.com>
2024-11-03 22:14:21 -0500

    fixed car edge of road

--- assets/js/audio-control.js
+++ assets/js/audio-control.js
@@ -692,10 +692,10 @@ document.addEventListener('DOMContentLoaded', async function() {
         value = Math.max(min, Math.min(max, value));
         
         // Adjust the available travel distance by subtracting the car width
-        const adjustedWidth = sliderWidth - 2.75*carWidth;
+        const adjustedWidth = sliderWidth - 2.9*carWidth;
     
         // Calculate position as a percentage of the adjusted width
-        const carPosition = Math.round(((value - min) / (max - min)) * adjustedWidth + carWidth/1.1);
+        const carPosition = Math.round(((value - min) / (max - min)) * adjustedWidth + carWidth);
     
         carElement.style.left = `${carPosition}px`;
     }
